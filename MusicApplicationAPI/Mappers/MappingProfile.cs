b5d901831e5d2a1ba653b33a8c32e7eec4c0cac4
Kousik <rajkousik20@gmail.com>
2024-07-23 23:59:28 +0530

    added: mapper for user and user DTOs

---
+++ MusicApplicationAPI/Mappers/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MusicApplicationAPI.Models.DbModels;
+using MusicApplicationAPI.Models.DTOs.UserDTO;
+
+namespace MusicApplicationAPI.Mappers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            #region User
+            CreateMap<User, UserRegisterDTO>().ReverseMap();
+            CreateMap<User, UserLoginDTO>().ReverseMap();
+            CreateMap<User, UserLoginReturnDTO>().ReverseMap();
+            CreateMap<User, UserRegisterReturnDTO>().ReverseMap();
+
+            CreateMap<UserRegisterDTO, UserLoginDTO>().ReverseMap();
+            CreateMap<UserRegisterDTO, UserLoginReturnDTO>().ReverseMap();
+            CreateMap<UserRegisterDTO, UserRegisterReturnDTO>().ReverseMap();
+
+            CreateMap<UserLoginDTO, UserLoginReturnDTO>().ReverseMap();
+            CreateMap<UserLoginDTO, UserRegisterReturnDTO>().ReverseMap();
+
+            CreateMap<UserRegisterReturnDTO, UserLoginReturnDTO>().ReverseMap();
+            #endregion
+        }
+    }
+}
