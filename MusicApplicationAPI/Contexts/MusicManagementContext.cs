f853217a1e317cda56cddfb0c050a1f3fe5aa692
Kousik <rajkousik20@gmail.com>
2024-07-23 21:26:47 +0530

    added: context

---
+++ MusicApplicationAPI/Contexts/MusicManagementContext.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicApplicationAPI.Models.DbModels;
+
+namespace MusicApplicationAPI.Contexts
+{
+    public class MusicManagementContext : DbContext
+    {
+        #region Constructor
+        public MusicManagementContext(DbContextOptions<MusicManagementContext> options) : base(options)
+        {
+        }
+        #endregion
+
+        #region DbSet Properties
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
+        public DbSet<PlaylistSong> PlaylistSongs { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Favorite> Favorites { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+
+        #endregion
+
+        #region OnModelCreating
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+
+            modelBuilder.Entity<PlaylistSong>()
+                .HasOne(ps => ps.Playlist)
+                .WithMany(p => p.PlaylistSongs)
+                .HasForeignKey(ps => ps.PlaylistId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<PlaylistSong>()
+                .HasOne(ps => ps.Song)
+                .WithMany(s => s.PlaylistSongs)
+                .HasForeignKey(ps => ps.SongId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Favorite>()
+                .HasOne(f => f.User)
+                .WithMany(u => u.Favorites)
+                .HasForeignKey(f => f.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Favorite>()
+                .HasOne(f => f.Song)
+                .WithMany(s => s.Favorites)
+                .HasForeignKey(f => f.SongId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Favorite>()
+                .HasOne(f => f.Playlist)
+                .WithMany(p => p.Favorites)
+                .HasForeignKey(f => f.PlaylistId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Rating>()
+                .HasOne(r => r.User)
+                .WithMany(u => u.Ratings)
+                .HasForeignKey(r => r.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Rating>()
+                .HasOne(r => r.Song)
+                .WithMany(s => s.Ratings)
+                .HasForeignKey(r => r.SongId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+        #endregion
+    }
+}
