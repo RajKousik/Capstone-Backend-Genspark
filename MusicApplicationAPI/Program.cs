3a45defe475756d2844406618b6cfe0dda6cd617
Kousik <rajkousik20@gmail.com>
2024-07-23 21:27:47 +0530

    added: watchdog & swagger configurations

--- MusicApplicationAPI/Program.cs
+++ MusicApplicationAPI/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using MusicApplicationAPI.Contexts;
+using System.Text;
+using WatchDog;
+
 namespace MusicApplicationAPI
 {
     public class Program
@@ -6,21 +14,91 @@ namespace MusicApplicationAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            #region Controllers
             // Add services to the container.
-
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            #endregion
+
+            #region Swagger Configurations
+
+            builder.Services.AddSwaggerGen(option =>
+            {
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                     {
+                           new OpenApiSecurityScheme
+                           {
+                                 Reference = new OpenApiReference
+                                 {
+                                     Type = ReferenceType.SecurityScheme,
+                                     Id = "Bearer"
+                                 }
+                           },
+                           new string[] {}
+                     }
+                });
+            });
+
+            #endregion
+
+            #region DbContexts
+
+            builder.Services.AddDbContext<MusicManagementContext>(
+                options => options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection"))
+                );
+
+            #endregion
+
+            #region Authentication
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey:JWT"]))
+                    };
+
+                });
+
+            #endregion
+
+            #region WatchDog
+
+            builder.Services.AddWatchDogServices(opt =>
+            {
+                opt.SetExternalDbConnString = builder.Configuration.GetConnectionString("WatchDogConnection");
+                opt.DbDriverOption = WatchDog.src.Enums.WatchDogDbDriverEnum.MSSQL;
+            });
+
+            #endregion
 
             var app = builder.Build();
 
+            // Configure the HTTP request pipeline.
+            #region Swagger Configurations
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+            #endregion
 
             app.UseHttpsRedirection();
 
@@ -28,6 +106,18 @@ namespace MusicApplicationAPI
 
 
             app.MapControllers();
+
+
+            #region WatchDog Configurations
+            app.UseWatchDogExceptionLogger();
+
+            var watchdogCredentials = builder.Configuration.GetSection("WatchDog");
+            app.UseWatchDog(opt =>
+            {
+                opt.WatchPageUsername = watchdogCredentials["username"];
+                opt.WatchPagePassword = watchdogCredentials["password"];
+            });
+            #endregion
 
             app.Run();
         }
