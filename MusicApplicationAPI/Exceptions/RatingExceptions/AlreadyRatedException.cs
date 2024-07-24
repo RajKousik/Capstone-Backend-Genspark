260feec56b5315a8c34e8fee2621d995f2361f4a
Kousik <rajkousik20@gmail.com>
2024-07-24 18:24:23 +0530

    added: Rating Exceptions

---
+++ MusicApplicationAPI/Exceptions/RatingExceptions/AlreadyRatedException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace MusicApplicationAPI.Exceptions.RatingExceptions
+{
+    [Serializable]
+    public class AlreadyRatedException : Exception
+    {
+        private string msg;
+
+        public AlreadyRatedException()
+        {
+            msg = "User has already rated this song.";
+        }
+
+        public AlreadyRatedException(string message) : base(message)
+        {
+            msg = message;
+        }
+
+        public override string Message => msg;
+    }
+}
---
+++ MusicApplicationAPI/Exceptions/RatingExceptions/UnableToRetrieveTopRatedSongsException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace MusicApplicationAPI.Exceptions.RatingExceptions
+{
+    [Serializable]
+    public class UnableToRetrieveTopRatedSongsException : Exception
+    {
+        private string msg;
+
+        public UnableToRetrieveTopRatedSongsException()
+        {
+            msg = "Unable To Retrieve The Top Rated Songs.";
+
+        }
+
+        public UnableToRetrieveTopRatedSongsException(string message) : base(message)
+        {
+            msg = message;
+        }
+
+        public override string Message => msg;
+    }
+}
