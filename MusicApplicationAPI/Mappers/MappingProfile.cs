f20eb2b42878ad2f7eef8bb147f45cb291080b20
Kousik <rajkousik20@gmail.com>
2024-07-24 14:57:25 +0530

    added: songs, playlist, playlist songs mappings

--- MusicApplicationAPI/Mappers/MappingProfile.cs
+++ MusicApplicationAPI/Mappers/MappingProfile.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
 using MusicApplicationAPI.Models.DbModels;
+using MusicApplicationAPI.Models.DTOs.PlaylistDTO;
+using MusicApplicationAPI.Models.DTOs.PlaylistSongDTO;
+using MusicApplicationAPI.Models.DTOs.SongDTO;
 using MusicApplicationAPI.Models.DTOs.UserDTO;
 
 namespace MusicApplicationAPI.Mappers
@@ -23,6 +26,41 @@ namespace MusicApplicationAPI.Mappers
 
             CreateMap<UserRegisterReturnDTO, UserLoginReturnDTO>().ReverseMap();
             #endregion
+
+            #region Song
+
+            CreateMap<Song, SongReturnDTO>().ReverseMap();
+            CreateMap<Song, SongUpdateDTO>().ReverseMap();
+            CreateMap<Song, SongAddDTO>().ReverseMap();
+            
+            CreateMap<SongReturnDTO, SongUpdateDTO>().ReverseMap();
+            CreateMap<SongReturnDTO, SongAddDTO>().ReverseMap();
+            
+            CreateMap<SongUpdateDTO, SongAddDTO>().ReverseMap();
+            #endregion
+
+            #region Playlist
+            CreateMap<Playlist, PlaylistAddDTO>().ReverseMap();
+            CreateMap<Playlist, PlaylistReturnDTO>().ReverseMap();
+            CreateMap<Playlist, PlaylistUpdateDTO>().ReverseMap();
+
+            CreateMap<PlaylistAddDTO, PlaylistReturnDTO>().ReverseMap();
+            CreateMap<PlaylistAddDTO, PlaylistUpdateDTO>().ReverseMap();
+
+            CreateMap<PlaylistReturnDTO, PlaylistUpdateDTO>().ReverseMap();
+            #endregion
+
+            #region Playlist Song
+
+            CreateMap<PlaylistSong, PlaylistSongDTO>().ReverseMap();
+            CreateMap<PlaylistSong, PlaylistSongReturnDTO>().ReverseMap();
+
+            CreateMap<PlaylistSongDTO, PlaylistSongReturnDTO>().ReverseMap();
+
+
+            #endregion
+
+
         }
     }
 }
